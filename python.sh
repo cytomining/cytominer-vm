a0dc41be312c60b0f8a21f30dc0296e26e7fb088
Beth Cimini <bethac07@users.noreply.github.com>
2021-06-18 16:14:25 -0400

    Update python.sh

--- python.sh
+++ python.sh
@@ -5,10 +5,7 @@
 #-----------------------------
 
 sudo apt-get install -y \
-    python \
-    python-dev \
-    python-pip \
-    python-setuptools
+    python3-pip
 
 #-----------------------------
 # pyenv
@@ -19,13 +16,13 @@ mkdir -p ~/work/software/archives
 
 cd ~/work/software/archives
 
-git clone https://github.com/yyuu/pyenv.git ~/.pyenv
+git clone https://github.com/pyenv/pyenv.git ~/.pyenv
 
-echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bash_profile
+echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 
-echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bash_profile
+echo 'export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 
-echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.bash_profile
+echo -e 'eval "$(pyenv init -)"' >> ~/.bashrc
 
 # In a non-interactive install, you can't do this:
 # exec "$SHELL"
@@ -37,9 +34,9 @@ export PATH="$PYENV_ROOT/bin:$PATH"
 
 eval "$(pyenv init -)"
 
-pyenv install 3.8.2
+pyenv install 3.8.10
 
-pyenv shell 3.8.2
+pyenv shell 3.8.10
 
 pip install --upgrade \
     pip \
@@ -50,57 +47,35 @@ pip install --upgrade \
 # pe2loaddata
 #-----------------------------
 
-# This section will go once pe2loaddata is packaged
+cd ~
 
-pip install --upgrade \
-    IPython \
-    pyyaml
+git clone https://github.com/broadinstitute/pe2loaddata.git
 
-#-----------------------------
-# NOTE:
-# DCP and cytominer are configured within this script because
-# pyenv wouldn't load when they were moved into their own scripts
-#-----------------------------
+pip3 install --upgrade \
+    click \
+    PyYAML
+    
+cd pe2loaddata
+
+pip3 install -e . 
+
 
 #-----------------------------
 # DCP Control Node
 # https://github.com/CellProfiler/Distributed-CellProfiler/wiki/Before-you-get-started%3A-setting-up
 #-----------------------------
 
-# This is done in tools.sh
-# sudo apt install build-essential checkinstall
-# sudo apt-get install parallel
-
-# This is done above
-# sudo apt install python python-dev python-setuptools python-dev build-essential
-# pip install --upgrade setuptools
-
-# This is no longer needed
-# See https://pip.pypa.io/en/stable/installing/
-# sudo easy_install pip
-
-sudo apt-get install -y \
-    cloud-image-utils \
-    fabric
-
 cd ~
 
 git clone https://github.com/CellProfiler/Distributed-CellProfiler.git
 
-cd Distributed-CellProfiler/files
-
-pyenv local 3.8.2
-
-pyenv shell 3.8.2
-
-pip install -r requirements.txt
-
-pip install --upgrade \
-    awscli
+pip3 install --upgrade \
+    awscli \
+    boto3
 
 #-----------------------------
-# cytominer
+# cytominer-database
 #-----------------------------
 
-pip install cytominer-database==0.3.3
+pip3 install cytominer-database
 
