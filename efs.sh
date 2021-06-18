443d55256000d1476855a4378051e1cdc2b60cfe
Shantanu Singh <shsingh@broadinstitute.org>
2021-06-18 18:59:53 -0400

    Update efs.sh

--- efs.sh
+++ efs.sh
@@ -13,4 +13,5 @@ sudo mkdir ~/efs
 
 sudo chown ubuntu ~/efs/
 
+# instructions from https://docs.aws.amazon.com/efs/latest/ug/wt1-test.html
 echo "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EFS_ID}.efs.us-east-1.amazonaws.com:/ ~/efs" >> ~/.bashrc
