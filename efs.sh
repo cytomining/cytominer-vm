04c5a71db2538a8134c7dedae09b23a69ad1f43e
Shantanu Singh <shsingh@broadinstitute.org>
2021-06-18 19:01:09 -0400

    Update efs.sh

--- efs.sh
+++ efs.sh
@@ -15,3 +15,6 @@ sudo chown ubuntu ~/efs/
 
 # instructions from https://docs.aws.amazon.com/efs/latest/ug/wt1-test.html
 echo "sudo mount -t nfs4 -o nfsvers=4.1,rsize=1048576,wsize=1048576,hard,timeo=600,retrans=2 $(curl -s http://169.254.169.254/latest/meta-data/placement/availability-zone).${EFS_ID}.efs.us-east-1.amazonaws.com:/ ~/efs" >> ~/.bashrc
+
+# TODO: There's a better way to do it 
+# https://docs.aws.amazon.com/efs/latest/ug/accessing-fs-nfs-permissions-per-user-subdirs.html#accessing-fs-nfs-permissions-per-user-subdirs-auto-mount-on-reboot
